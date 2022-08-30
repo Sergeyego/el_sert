4a70d5714da2f710d4a6b4ecbdaf58fe2dd06368
Sergeyego <sergen1@rambler.ru>
2022-08-30 10:53:58 +0300

    добавлен код положения при сварке

--- formpos.cpp
+++ formpos.cpp
@@ -10,11 +10,13 @@ FormPos::FormPos(QWidget *parent) :
     modelPos = new DbTableModel("pics",this);
     modelPos->addColumn("id","id");
     modelPos->addColumn("descr",QString::fromUtf8("Описание"));
+    modelPos->addColumn("dt",QString::fromUtf8("Код"));
     modelPos->setSort("pics.descr");
     modelPos->select();
     ui->tableViewPos->setModel(modelPos);
     ui->tableViewPos->setColumnHidden(0,true);
     ui->tableViewPos->setColumnWidth(1,400);
+    ui->tableViewPos->setColumnWidth(2,80);
 
     connect(ui->tableViewPos->selectionModel(),SIGNAL(currentRowChanged(QModelIndex,QModelIndex)),this,SLOT(loadImg(QModelIndex)));
     connect(ui->cmdLoad,SIGNAL(clicked(bool)),this,SLOT(newImg()));
