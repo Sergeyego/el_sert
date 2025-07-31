b8f26347d02864b9295463b0079570ec189cf4bb
Сергей <sergej@Mac-mini-Sergej.local>
2025-07-31 10:13:09 +0300

    fix geometry

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -137,11 +137,9 @@ void MainWindow::partReq(int /*id_part*/, QString prefix)
 void MainWindow::loadSettings()
 {
     QSettings settings("szsm", QApplication::applicationName());
-    this->restoreGeometry(settings.value("main_geometry").toByteArray());
     this->restoreState(settings.value("main_state").toByteArray());
     QString opentab=settings.value("main_opentab").toString();
     QString current=settings.value("main_currenttab").toString();
-
     if (opentab.isEmpty()){
         ui->actionPartEl->trigger();
     } else {
@@ -153,6 +151,7 @@ void MainWindow::loadSettings()
         }
     }
     setActiveSubWindow(current);
+    this->restoreGeometry(settings.value("main_geometry").toByteArray());
 }
 
 void MainWindow::saveSettings()
